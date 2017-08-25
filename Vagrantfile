cd63e40e0f46c62486c7ae9d94045da4f42545cd
Joyce <AJOYCE@corp.vha.ad>
2017-08-25 09:46:21 -0500

    Povisions correctly

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,7 @@
 
 Vagrant.configure("2") do |config|
 
-    config.vm.box = "scotch/box"
+    config.vm.box = "ubuntu/xenial64"
     config.vm.network "private_network", ip: "192.168.33.10"
     config.vm.network "forwarded_port", guest: 5000, host: 5000
     config.vm.hostname = "scotchbox"
@@ -16,5 +16,5 @@ Vagrant.configure("2") do |config|
     # Optional NFS. Make sure to remove other synced_folder line too
     #config.vm.synced_folder ".", "/var/www", :nfs => { :mount_options => ["dmode=777","fmode=666"] }
 
-    config.vm.provision :shell, inline: "echo \"cd ../../var/www\" >> /home/vagrant/.bashrc"
+    config.vm.provision :shell, inline: "echo \"cd ../../var/www\" >> /home/ubuntu/.bashrc"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
+case $(id -u) in
+    0) 
+        echo first: running as root
+        echo doing the root tasks...
+        sudo apt-get install curl software-properties-common python-software-properties npm git -y
+        sudo -u ubuntu -i $0  # script calling itself as the vagrant user
+        ;;
+    *) 
+        echo then: running as vagrant user
+        echo doing the vagrant users tasks
+        # install latest nvm
+        git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
+        source ~/.nvm/nvm.sh
+        echo "source ~/.nvm/nvm.sh" >> ~/.bashrc
 
-sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
-curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install heroku expect -y
+        # install latest stable node.js
+        echo "Installing node.js... (please be patient)"
+        nvm install stable &> /dev/null
+        nvm alias default stable
 
-echo APT-GET INSTALL INSTALL BUILD-ESSENTIAL, CHECKINSTALL, LIBSSL-DEV
-sudo apt-get install build-essential checkinstall libssl-dev
+        echo INSTALLING ANGULAR-CLI
+        npm install -g @angular/cli@latest typescript@latest --save
 
-echo INSTALLING NVM
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+        echo STARTING HEROKU INSTALLATION
+        sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+        curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+        sudo apt-get update
+        sudo apt-get install heroku expect -y
+        ;;
+esac
 
-echo INSTALLING NPM
-npm install -g npm
 
-echo INSTALLING NODE 8.4.0
-nvm install 8.4.0
-nvm use 8.4.0
-nvm alias default 8.4.0
-
-echo INSTALLING ANGULAR-CLI
-npm install -g @angular/cli@latest typescript@latest --save
 
